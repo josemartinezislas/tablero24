874464643250e950c00ea897f24aa3daa925a11a
josemartinez <jose.martinez.islas@gmail.com>
2024-08-06 18:38:39 -0600

    Revert "plantilla dividida en /include, scrips de consultas de tablas INNER JOIN, Query tabla de resultados"
    This reverts commit d44286cc08f397d962127bd4864b16a6c8274216.

--- ind.php
+++ ind.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 //con esto muestra la información pricipal de la version de php
 echo phpinfo();
 echo ('cambios modificados en vs code!!');
@@ -16,3 +17,6 @@ echo "<h2>$txt1</h2>";
 echo "<p>Study PHP at $txt2</p>";
 
 // Termina echo variables! tercer commit=============
+=======
+echo phpinfo();
+>>>>>>> parent of d44286c (plantilla dividida en /include, scrips de consultas de tablas INNER JOIN, Query tabla de resultados)
